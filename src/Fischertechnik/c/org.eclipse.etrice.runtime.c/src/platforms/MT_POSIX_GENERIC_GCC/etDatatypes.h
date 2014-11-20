500a8afbcce66401f679cd9632b387549c0549f4
Thomas Schuetz <ts@protos.de>
2014-11-20 23:46:11 +0100

    [Fischertechnik] added runtime libraries

---
+++ src/Fischertechnik/c/org.eclipse.etrice.runtime.c/src/platforms/MT_POSIX_GENERIC_GCC/etDatatypes.h
@@ -0,0 +1,116 @@
+/*******************************************************************************
+ * Copyright (c) 2011 protos software gmbh (http://www.protos.de).
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * CONTRIBUTORS:
+ * 		Thomas Schuetz (initial contribution)
+ * 		Henrik Rentz-Reichert
+ *
+ *******************************************************************************/
+
+#ifndef _ETDATATYPES_H_
+#define _ETDATATYPES_H_
+
+/*
+ * typedefs for platform specific datatypes
+ * POSIX version
+ *
+ * */
+
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <signal.h>
+#include <sys/types.h>
+
+/* unsigned integer datatypes */
+typedef unsigned char uint8;
+typedef unsigned short int uint16;
+typedef unsigned long uint32;
+typedef unsigned long long uint64;
+
+/* signed integer datatypes */
+typedef char int8;
+typedef short int int16;
+typedef long int32;
+typedef long long int64;
+
+
+/* float datatypes */
+typedef float float32;
+typedef double float64;
+
+/* string datatypes */
+typedef char* charPtr;
+
+
+#ifndef NULL
+	#define NULL 0
+#endif
+
+/* boolean datatypes and values */
+typedef char bool;  	/* TODO: bool, Bool, Boolean, and boolean are already defined in some platforms*/
+typedef bool boolean;
+
+#ifndef ET_TRUE
+	#define ET_TRUE 1
+#endif
+#ifndef ET_FALSE
+	#define ET_FALSE 0
+#endif
+#ifndef true
+	#define true 1
+#endif
+#ifndef false
+	#define false 0
+#endif
+
+#define ALIGNMENT		8	/* power of 2 and >= sizeof(int) ! */
+
+/*
+ * typedefs for eTrice Runtime and Testing
+ *
+ * */
+
+typedef int8 etInt8;
+typedef int16 etInt16;
+typedef int32 etInt32;
+
+typedef uint8 etUInt8;
+typedef uint16 etUInt16;
+typedef uint32 etUInt32;
+
+typedef charPtr etCharPtr;
+
+typedef float32 etFloat32;
+typedef float64 etFloat64;
+
+typedef bool etBool;
+
+
+typedef FILE* etFileHandle;
+
+typedef int16 etAddressId;
+
+/*
+ * typedefs for OS-specific types
+ */
+
+typedef pthread_mutex_t etOSMutexData;
+typedef pthread_t etOSThreadData;
+typedef pthread_t etOSThreadId;
+typedef sem_t etOSSemaData;
+typedef struct {
+	void* next;
+	timer_t timerid;
+	sigevent_t te;
+	etBool signaled;
+}
+etOSTimerData;
+typedef timer_t etOSTimerId;
+
+
+#endif /* _DATATYPES_H_ */
